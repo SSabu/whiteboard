7ffd2cb6b9e41bb71a88a3f88170d1b92b5aeec7
SSabu <sandeepsabu09@gmail.com>
2017-06-12 13:32:35 -0700

    RefactorQueue first commit

---
+++ src/Queue.js
@@ -0,0 +1,22 @@
+const Stack = require('./Stack');
+
+class Queue {
+  constructor () {
+    this._stackOne = new Stack();
+    this._stackTwo = new Stack();
+  }
+
+  enqueue () {
+
+  }
+
+  dequeue () {
+
+  }
+
+  size () {
+
+  }
+}
+
+module.exports = Queue;
